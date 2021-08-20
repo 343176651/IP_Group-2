ee6789a2078372657207c1321b53137262f68cd4
Zheng Liang <343176651@qq.com>
2021-08-20 13:55:29 +0800

    优化，新增checkinput

--- GameDemo.py
+++ GameDemo.py
@@ -1,7 +1,9 @@
 import random
+import re
 
 
-class UnitCreate:
+class UnitCreated:
+    id = 0
     name = ""
     health = 100
     experience = 0
@@ -11,7 +13,8 @@ class UnitCreate:
     type = 0  # 0 = tanker 1 = warrior
     profession = ""
 
-    def __init__(self, name, type):
+    def __init__(self, name, type, id):
+        self.id = id
         self.name = name
         self.type = type
         if self.type == 0:
@@ -24,9 +27,23 @@ class UnitCreate:
             self.profession = "Warrior"
 
     def print_attr(self):
-        print("NAME: {},profession: {},HP: {},EXP: {},RANK: {},ATK: {},DEF: {}"
-              .format(self.name, self.profession, self.health, self.experience,
+        print("[ID:{}, NAME:{}, profession:{}, HP:{}, EXP:{}, RANK:{}, ATK:{}, DEF:{}]"
+              .format(self.id, self.name, self.profession, self.health, self.experience,
                       self.rank, self.attack, self.defence))
+
+
+class CheckUid:
+    uid = 0
+
+    @classmethod
+    def check(cls, uid_chosen):
+        pattern = re.compile('^([0-3])$')
+        if pattern.search(uid_chosen) is not None:
+            cls.uid = int(uid_chosen)
+            return cls.uid
+        else:
+            cls.uid = random.randint(1, 3)
+            return cls.uid
 
 
 def main():
@@ -35,24 +52,41 @@ def main():
     attr = []
     en_units = []
     en_attr = []
-    for i in range(3):
-        type = int(input('Please choose a profession for your unit("0" for Tanker,"1" for Warrior): '))
+    for i in range(3):  # create 3 units
+
+        user_input = input('Please choose a profession for your unit("0" for Tanker,"1" for Warrior): ')
+
+        if user_input == "1" or user_input == "0":
+            type = int(user_input)
+            pass
+        else:
+            type = random.randint(0, 1)
+            input("Invalid input!! Random profession is created.\n " +
+                  "===press ENTER to continue===")
+
         name = input('Please name your unit:')
-        attr.append((name, type))
-        units.append(UnitCreate(attr[i][0], attr[i][1]))
+        uid = i + 1
+        attr.append((name, type, uid))
+        units.append(UnitCreated(attr[i][0], attr[i][1], attr[i][2]))
+        UnitCreated.print_attr(units[i])
     print("===player's team===")
     for i in range(len(units)):
-        UnitCreate.print_attr(units[i])
-    input("===Press enter to continue===")
+        UnitCreated.print_attr(units[i])
+    input("===Press ENTER to continue===")
 
     for i in range(3):
         en_type = random.randint(0, 1)
         en_name = "AI" + str(random.randint(10, 99))
-        en_attr.append((en_name, en_type))
-        en_units.append(UnitCreate(en_attr[i][0], en_attr[i][1]))
+        en_id = i + 1
+        en_attr.append((en_name, en_type, en_id))
+        en_units.append(UnitCreated(en_attr[i][0], en_attr[i][1], attr[i][2]))
     print("===enemy team===")
     for i in range(len(en_units)):
-        UnitCreate.print_attr(en_units[i])
+        UnitCreated.print_attr(en_units[i])
+
+    uid_chosen = input("please choose your unit by enter his/her id :")
 
 
-main()
+
+if __name__ == "__main__":
+    main()
--- Test code.py
+++ Test code.py
@@ -1,4 +1,9 @@
-print("hello")
-a = 1 + 1
-print(a)
-print('hello')
+import random
+for i in range(10) :
+    print(random.randint(1,3))
+
+import re
+pattern = re.compile('^([0-3])$')
+str = '3'
+print(pattern.search(str))
+print(5)
