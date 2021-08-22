dff347e9a5531d89c262e11fba8e2eda665437f1
Zheng Liang <343176651@qq.com>
2021-08-22 14:45:52 +0800

    unitCreated变为外部类，设置get/set 调用私有变量

--- GameDemo.py
+++ GameDemo.py
@@ -1,51 +1,23 @@
 import random
 import re
+import UnitCreated
+
 units = []
 en_units = []
-
-
-class UnitCreated:
-    id = 0
-    name = ""
-    health = 100
-    experience = 0
-    rank = 1
-    attack = 0
-    defence = 0
-    type = 0  # 0 = tanker 1 = warrior
-    profession = ""
-
-    def __init__(self, name, type, id):
-        self.id = id
-        self.name = name
-        self.type = type
-        if self.type == 0:
-            self.attack = random.randint(1, 10)
-            self.defence = random.randint(5, 15)
-            self.profession = "Tanker"
-        else:
-            self.attack = random.randint(5, 20)
-            self.defence = random.randint(1, 10)
-            self.profession = "Warrior"
-
-    def print_attr(self):
-        print("[ID:{}, NAME:{}, profession:{}, HP:{}, EXP:{}, RANK:{}, ATK:{}, DEF:{}]"
-              .format(self.id, self.name, self.profession, self.health, self.experience,
-                      self.rank, self.attack, self.defence))
 
 
 class CheckUid:
     uid = 0
 
     @classmethod
-    def check(cls, uid_chosen):
-        pattern = re.compile('^([0-3])$')
-        if pattern.search(uid_chosen) is not None:
-            cls.uid = int(uid_chosen)
-            return cls.uid
+    def check(cls, id_chosen):
+        pattern = re.compile('^([1-3])$')
+        if pattern.search(id_chosen) is not None:
+            cls.uid = int(id_chosen)
+            return print("ID({}) is selected".format(cls.uid))
         else:
             cls.uid = random.randint(1, 3)
-            return cls.uid
+            return print("Invalid ID, random ID({}) is selected".format(cls.uid))
 
 
 def main():
@@ -53,7 +25,9 @@ def main():
     en_attr = []
     print("===Create your unit===")
     global units
-    for i in range(3):  # create 3 units
+
+    # create player's units
+    for i in range(3):
 
         user_input = input('Please choose a profession for your unit("0" for Tanker,"1" for Warrior): ')
 
@@ -68,24 +42,48 @@ def main():
         name = input('Please name your unit:')
         uid = i + 1
         attr.append((name, type, uid))
+
         units.append(UnitCreated(attr[i][0], attr[i][1], attr[i][2]))
+
+        # show unit's attribute
         UnitCreated.print_attr(units[i])
+
     print("===player's team===")
+
+    # print player's whole team
     for i in range(len(units)):
         UnitCreated.print_attr(units[i])
+
     input("===Press ENTER to continue===")
     global en_units
+
+    # create enemy team
     for i in range(3):
         en_type = random.randint(0, 1)
         en_name = "AI" + str(random.randint(10, 99))
         en_id = i + 1
         en_attr.append((en_name, en_type, en_id))
         en_units.append(UnitCreated(en_attr[i][0], en_attr[i][1], attr[i][2]))
+
     print("===enemy team===")
+
+    # print enemy team
     for i in range(len(en_units)):
         UnitCreated.print_attr(en_units[i])
 
+    input("===Press ENTER to continue===")
+
     uid_chosen = input("please choose your unit by enter his/her id :")
+
+    CheckUid.check(uid_chosen)  # check input
+
+    UnitCreated.print_attr(units[int(uid_chosen - 1)])  # index of list = id - 1
+
+    eid_chosen = input("please choose enemy unit you want to attack by enter his/her id :")
+
+    CheckUid.check(eid_chosen)
+
+    UnitCreated.print_attr(units[int(eid_chosen - 1)])
 
 
 if __name__ == "__main__":
---
+++ UnitCreated.py
@@ -0,0 +1,95 @@
+import random
+
+
+class UnitCreated:
+
+    def get_id(self):
+        return self.__id
+
+    def set_id(self, id):
+        self.__id = id
+
+    id = property(get_id, set_id)
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    name = property(get_id, set_id)
+
+    def get_health(self):
+        return self.__health
+
+    def set_health(self, health):
+        self.__health = health
+
+    health = property(get_health, set_health)
+
+    def get_exp(self):
+        return self.__experience
+
+    def set_exp(self, exp):
+        self.__experience = exp
+
+    experience = property(get_exp, set_exp)
+
+    def get_rank(self):
+        return self.__rank
+
+    def set_rank(self, rank):
+        self.__rank = rank
+
+    rank = property(get_rank, set_rank)
+
+    def get_atk(self):
+        return self.__attack
+
+    def set_atk(self, atk):
+        self.__attack = atk
+
+    attack = property(get_atk, set_atk)
+
+    def get_def(self):
+        return self.__defence
+
+    def set_def(self, defence):
+        self.__defence = defence
+
+    defence = property(get_def, set_def)
+
+    def get_type(self):
+        return self.type
+
+    type = property(get_type)  # 0 = tanker 1 = warrior
+
+    def get_pro(self):
+        return self.__profession
+
+    profession = property(get_pro)
+
+    def __init__(self, name, type, id):
+        self.__id = id
+        self.__name = name
+        self.__type = type
+        self.__health = 100
+        self.__experience = 0
+        self.__rank = 1
+        if self.__type == 0:
+            self.__attack = random.randint(1, 10)
+            self.__defence = random.randint(5, 15)
+            self.__profession = "Tanker"
+        else:
+            self.__attack = random.randint(5, 20)
+            self.__defence = random.randint(1, 10)
+            self.__profession = "Warrior"
+
+    def print_attr(self):
+        print("[ID:{}, NAME:{}, profession:{}, HP:{}, EXP:{}, RANK:{}, ATK:{}, DEF:{}]"
+              .format(self.__id, self.__name, self.__profession, self.__health, self.__experience,
+                      self.__rank, self.__attack, self.__defence))
+
+    def state_check(self):
+        if self.__health > 0:
+            pass  # not finished
